c6659ccb59d5efc6039bb67fdab602f263bbfa9f
kila <luka.kilasonia.1@btu.edu.ge>
2025-01-15 17:28:39 +0400

    create user profile model

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,8 +5,22 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
-    email = models.CharField(unique=True)
+    email = models.CharField(max_length=500, unique=True)
     full_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='images', blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    about = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    author = models.BooleanField(default=False)
+    country = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.user.username
