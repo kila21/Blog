1d3842cdb533c540b69fe5350ebbf755a9f2d2ff
kila <luka.kilasonia.1@btu.edu.ge>
2025-01-15 22:24:43 +0400

    [models] create post model

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -34,3 +35,21 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     
+
+## posts have categories to filter.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to='images/category')
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+    def save(self, *args, **kwargs):
+        if self.slug == '' or self.slug == None:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+# post Model
+
