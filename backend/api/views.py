479b0353afd9787d8b799fa1d7b679cf305870ae
kila <luka.kilasonia.1@btu.edu.ge>
2025-02-25 02:40:40 +0400

    [views] create token and register view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+from api import serializer as api_serializer
+from api import models as api_models
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyCustomToken
+
+
+class RegisterView(generics.CreateAPIView)
+    queryset = api_models.User.objects.all()
+    serializer_class = api_serializer.RegisterSerializer
+    permission_classes = [AllowAny]
+    
