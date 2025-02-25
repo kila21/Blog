68143a66d2ea5d13420a1fce33e8c518f875ed69
kila <luka.kilasonia.1@btu.edu.ge>
2025-02-25 17:19:09 +0400

    [urls] new urls for api views

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from api import views as api_views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('user/token/', api_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
+    path('user/register', api_views.RegisterView.as_view()),
+    path('user/profile/', api_views.ProfileView.as_view())
+]
