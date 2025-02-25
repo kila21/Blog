713bffdc5b6c8515fb572dc138dcc62b88e2a147
kila <luka.kilasonia.1@btu.edu.ge>
2025-02-26 03:42:28 +0400

    [bug] update profile url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('user/token/', api_views.MyTokenObtainPairView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', api_views.RegisterView.as_view()),
-    path('user/profile/', api_views.ProfileView.as_view())
+    path('user/profile/<user_id>/', api_views.ProfileView.as_view())
 ]
