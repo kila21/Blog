fa91e175a67609fce548844a5bb2d56a836ed6d0
kila <luka.kilasonia.1@btu.edu.ge>
2025-03-01 17:20:44 +0400

    [urls] create post urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('user/token/', api_views.MyTokenObtainPairView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', api_views.RegisterView.as_view()),
-    path('user/profile/<user_id>/', api_views.ProfileView.as_view())
+    path('user/profile/<user_id>/', api_views.ProfileView.as_view()),
+
+    # Post Endpoints
+    path('post/category/list/', api_views.CategoryListApiView.as_view()),
+    path('post/category/posts/<category_slug>/', api_views.PostCategoryListApiView.as_view),
 ]
