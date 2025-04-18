956726a591ed4a1f0c895e8bab751b2e65d4c87a
kila <luka.kilasonia.1@btu.edu.ge>
2025-04-19 02:12:39 +0400

    [urls] change order of paths

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -30,7 +30,6 @@ schema_view = get_schema_view(
         default_version="v1",
         description="This is the documentation for the backend API",
         terms_of_service="http://mywbsite.com/policies/",
-        contact=openapi.Contact(email="desphixs@gmail.com"),
         license=openapi.License(name="BSD Licence"),
     ),
     public=True,
@@ -38,7 +37,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
 ]
